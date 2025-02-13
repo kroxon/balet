17f3ad9a9721bdf1066a4221f4c62349b7672602
kroxon <kadetkroksapsp2@gmail.com>
2025-02-13 23:05:38 +0100

    Create burger menu
    Created a burger menu for mobile devices with the same functionality as the desktop menu.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,22 @@
 import '../styles.css';
 
+function setupNavbar() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    burgerMenu.addEventListener('click', () => {
+        if (navLinks.classList.contains('nav-active')) {
+            navLinks.classList.remove('nav-active');
+        } else {
+            navLinks.classList.add('nav-active');
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
-    // zmienSekcje  
+
+    setupNavbar();
 
     // initializeGaleria();
     // initializeBlog();
