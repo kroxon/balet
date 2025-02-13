e78dab15a0446b5277f8c2c535d6c54dc29afea1
kroxon <kadetkroksapsp2@gmail.com>
2025-02-13 12:14:09 +0100

    Create basic structure of the project
    Add basic structure of the project and add divided js files for each page

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import '../styles.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+    // zmienSekcje('home');  
+
+    // initializeGaleria();
+    // initializeBlog();
+    // applyAnimations();
+});
